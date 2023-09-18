0b20ebe114b95c0ae93bc7f0741000cd33c8b4a4
LautaroJLZ <lautarojoseluis1234@gmail.com>
2023-09-18 00:29:32 -0300

    add some change to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <div className='c-navbar'>
+          <img src="src/views/img/logo-page.png" alt="Logo" />  
+          <div className='c-list-navbar'>
+            <ul className='list-navbar'>
+                <li>A CERCA DE MI</li>
+                <li>EXPERIENCIAS</li>
+                <li>PROJECTOS</li>
+                <li>CANTACTO</li>
+                <li>Soy Martino</li>
+            </ul>
+          </div>
+        </div>
     )
 }
 
